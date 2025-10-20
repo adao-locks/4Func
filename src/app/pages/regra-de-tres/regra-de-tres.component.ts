d819253253bdc4d1c4c1f434e70aab16fa6dc14e
Adao - MacOS <androydea9@gmail.com>
2025-10-19 21:11:04 -0300

    feat: Refactor Regra de Três component for improved functionality and structure

--- src/app/pages/regra-de-tres/regra-de-tres.component.ts
+++ src/app/pages/regra-de-tres/regra-de-tres.component.ts
@@ -1,29 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-regra-de-tres',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './regra-de-tres.component.html',
-  styleUrl: './regra-de-tres.component.css'
+  styleUrls: ['./regra-de-tres.component.css']
 })
 export class RegraDeTresComponent {
-  valor1 = '';
-  valor2 = '';
-  valor3 = '';
-  resultadoRT = '';
+  valor1 = 0;
+  valor2 = 0;
+  valor3 = 0;
+  resultadoRT: number | string = 0;
 
   calcularRegraDeTres() {
-    const num1 = parseFloat(this.valor1);
-    const num2 = parseFloat(this.valor2);
-    const num3 = parseFloat(this.valor3);
+    const num1 = Number(this.valor1);
+    const num2 = Number(this.valor2);
+    const num3 = Number(this.valor3);
 
-    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || num1 === 0) {
-      this.resultadoRT = 'Entrada inválida';
+    if (!num1 || isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+      this.resultadoRT = 0;
       return;
     }
 
-    const resultado = (num2 * num3) / num1;
-    this.resultadoRT = resultado.toString();
+    this.resultadoRT = (num2 / num1) * num3;
   }
 }
